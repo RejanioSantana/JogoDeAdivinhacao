fd0503c5a433b333188df3d24d3156dbff122d96
RejanioSantana <jhunior.gt@gmail.com>
2024-12-14 11:14:25 -0300

    Hello world!

---
+++ index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(){
+    srand(time(NULL));
+    int numeroAleatorio = rand() % 100 + 1;
+    int numeroUsuario;
+    int tentativa = 0;
+    //aprensentação do jogo
+    printf("\n\n\nBem-vindo ao jogo de Adivinhha!\n\nTente adivinhar...");
+
+    while (1)
+    {
+        tentativa++;
+        printf("Digite um número de 1 a 100: ");
+        scanf("%d",&numeroUsuario);
+
+        if (numeroUsuario == numeroAleatorio)
+        {   
+            printf("\nParabêns você acertou!\n");
+            printf("Numero de tentativas: %d\n\n", tentativa);
+            break;
+        }
+
+        if (numeroUsuario > numeroAleatorio)
+        {
+            printf("\n\nO numero é menor que %d\n\n",numeroUsuario);
+        }else{
+            printf("\n\nO numero é maior que %d\n\n",numeroUsuario);
+        }
+        
+        continue;
+        
+        
+    }
+    
+    return 0;
+}
